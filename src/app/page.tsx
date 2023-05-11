9f9ecc95a59e8ba1175ea75cf7da98cc83fcbfb7
Eli Brown-Henderson <Eli.i.henderson95@gmail.com>
2023-05-11 13:09:30 -0400

    UPDATE: loading spinner margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -135,7 +135,7 @@ export default function Home() {
 
             {loading && (
               <div className="flex justify-center items-center">
-                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
+                <div className="animate-spin rounded-full h-32 w-32 mt-3 border-t-2 border-b-2 border-white"></div>
               </div>
             )}
           </div>
